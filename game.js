f8adea8a6ba5426c27ba8eaeb0204c78d182adfe
asharghi <a.sharghi.mail@gmail.com>
2019-10-12 21:22:49 +0200

    Swipe support for mobile

--- game.js
+++ game.js
@@ -3,23 +3,38 @@ const initTetris = ({ width, height, pixelSize }) => {
     let position = startPosition;
     let rotation = 0, currentScore = 0;
     let currentBlock = false, nextBlock = false;
-    const UpdateScore = () => { document.querySelector(".tetris-score").innerHTML = currentScore; }
-    const clearBoard = () => { document.querySelectorAll(".pixel-fill").forEach(node => { node.classList.remove("pixel-fill") }); }
+
+    const clearBoard = () => { 
+        document.querySelectorAll(".pixel-fill").forEach(node => { node.classList.remove("pixel-fill") });
+    }
+
     const renderBlock = (block, rotation, position) => {
         clearBoard();
         block[rotation](position).forEach(index => {
             document.querySelectorAll(".tetris-pixel")[index].classList.add('pixel-fill');
         });
     }
+
     const freezeBlock = (block, rotation, position) => {
         clearBoard();
         block[rotation](position).forEach(index => { document.querySelectorAll(".tetris-pixel")[index].classList.add('pixel-stop'); });
     }
+
     const moveIsAllowed = (block, rotation, position) => {
         return !block[rotation](position).some(index => {
             return document.querySelectorAll(".tetris-pixel")[index].classList.contains('pixel-stop');
         });
     }
+
+    const SetScore = () => { 
+        let gameNode = document.querySelector(".tetris-game");
+        gameNode.removeChild(document.querySelector(".tetris-score"));
+        let scoreNode = document.createElement('div');
+        scoreNode.className = "tetris-score";
+        scoreNode.innerHTML = currentScore;
+        gameNode.appendChild(scoreNode);
+    }
+
     const AddNewLineAtTop = () => {
         var secondLineNode = document.querySelectorAll(".tetris-pixel")[width];
         for (var i = 0; i < width; i++) {
@@ -29,6 +44,7 @@ const initTetris = ({ width, height, pixelSize }) => {
         }
         secondLineNode.parentNode.insertBefore(document.createElement('br'), secondLineNode);
     }
+
     const RemoveFullLines = () => {
         let pixelNodes = document.querySelectorAll(".tetris-pixel");
         const hasClass = (element, className) => { return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1; }
@@ -53,39 +69,16 @@ const initTetris = ({ width, height, pixelSize }) => {
             }
         }
         currentScore += { 0: 0, 1: 40, 2: 100, 3: 300, 4: 1200 }[numberOfLinesRemoved];
-        UpdateScore();
+        SetScore();
     }
     const AddStyle = () => {
         var node = document.createElement('style');
         node.innerHTML = `
-        .tetris-pixel{
-          width:${pixelSize}px;
-          height:${pixelSize}px;
-          display:inline-block;
-          box-sizing: border-box;
-        }
-        .tetris-score{
-          color: #37a48b;
-          font-size: 3em;
-          margin-top: 1em;
-          text-align: center;
-          font-family: monospace;
-        }
-        .tetris-pixel.pixel-stop{
-          background: #37a48b;
-          opacity:0.3
-        }
-        .tetris-pixel.pixel-fill{
-          background: #37a48b;
-        }
-        .tetris-game {
-          line-height: 0;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          z-index: 99999;
-          transform: translate(-50%, -50%);
-        }`;
+        .tetris-pixel{ width:${pixelSize}px; height:${pixelSize}px; display:inline-block; box-sizing: border-box; }
+        .tetris-score{ color: #37a48b; font-size: 3em; margin-top: 1em; text-align: center; font-family: monospace; }
+        .tetris-pixel.pixel-stop{ background: #37a48b; opacity:0.3 }
+        .tetris-pixel.pixel-fill{ background: #37a48b; }
+        .tetris-game {  line-height: 0; position: absolute; top: 50%; left: 50%; z-index: 99; transform: translate(-50%, -50%); }`;
         document.body.appendChild(node);
     }
     const AddHtml = () => {
@@ -105,7 +98,10 @@ const initTetris = ({ width, height, pixelSize }) => {
         scoreNode.innerHTML = "0";
         gameNode.appendChild(scoreNode);
     }
-    AddHtml(); AddStyle();
+
+    AddHtml();
+    AddStyle();
+
     const blockTypes = [
         /*J*/[(p) => { return [p - width, p, p + width, p + width - 1] },
         (p) => { p = ((p - 2) % width) === 0 ? p + 1 : p; return [p - 2, p - 1, p, p + width] },
@@ -132,6 +128,7 @@ const initTetris = ({ width, height, pixelSize }) => {
         /*I*/[(p) => { return [p - width, p, p + width, p + width + width] },
         (p) => { p = ((p - 1) % width) === 0 || ((p - 2) % width) === 0 || ((p - 3) % width) === 0 ? p + 3 : p; return [p - 3, p - 2, p - 1, p] }]
     ];
+
     let player = setInterval(() => {
         currentBlock = currentBlock || blockTypes[Math.floor(Math.random() * blockTypes.length)];
         nextBlock = nextBlock || blockTypes[Math.floor(Math.random() * blockTypes.length)];
@@ -154,6 +151,7 @@ const initTetris = ({ width, height, pixelSize }) => {
             }
         }
     }, 1000);
+
     document.onkeydown = e => {
         let tempMove = { rotation, position }
         switch (e.keyCode) {
@@ -197,4 +195,6 @@ const initTetris = ({ width, height, pixelSize }) => {
                 break;
         }
     }
-};initTetris({ width: 10, height: 20, pixelSize: 35 });
+};
+
+initTetris({ width: 10, height: 20, pixelSize: 35 });
--- mobile-support.js
+++ mobile-support.js
@@ -1,19 +1,24 @@
-//This script is only needed for mobile. Adding invisible buttons on screen that triggers arrow key presses
+//This script is only needed for mobile. Adding swipe events for triggering arrow key presses
+let swipeElement = document.createElement("div");
+swipeElement.className = "tetris-swipe-element";
+swipeElement.style = "position: absolute; left: 0; top: 0; width: 100%; height: 100%; z-index: 999;";
 
-const keys = [{ keyCode: 37, keyName: "left" }, 
-              { keyCode: 38, keyName: "up" },
-              { keyCode: 39, keyName: "right" }, 
-              { keyCode: 40, keyName: "down" }];
+const triggerKeyPress = keyCode => {
+  document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': keyCode, 'which': keyCode }));
+};
 
-let hiddenButtons = document.createElement("style");
-hiddenButtons.innerHTML = ".tetris-left-click-area {top: 35%;left: 0;width: 50%;height: 35%;}.tetris-right-click-area {top: 35%;left: 50%;width: 50%;height: 35%;}.tetris-up-click-area {top: 0;left: 0;width: 100%;height: 35%;}.tetris-down-click-area {top: 70%;left: 0;width: 100%;height: 30%;}.click-area{z-index: 9999;position: absolute;}*{touch-action: manipulation;-webkit-touch-callout: none;-webkit-user-select: none;}"
-document.body.appendChild(hiddenButtons);
-
-keys.forEach(k => {
-  let node = document.createElement("div");
-  node.className = "tetris-" + k.keyName + "-click-area click-area";
-  node.onmousedown = () => {
-    document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': k.keyCode , 'which': k.keyCode }));
-  };
-  document.body.appendChild(node);
+[{ keyCode: 37, eventName: "swipeleft" },
+{ keyCode: 38, eventName: "swipeup" },
+{ keyCode: 39, eventName: "swiperight" },
+{ keyCode: 40, eventName: "swipedown" }].forEach(s => {
+  swipeElement.addEventListener(s.eventName, (e) => {
+    console.log(e.distance);
+    triggerKeyPress(s.keyCode);
+  });
 });
+
+document.body.appendChild(swipeElement);
+
+
+
+
